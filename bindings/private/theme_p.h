dabacfabfd43cec1fa4e03c7a40882ae41136b2a
Marco Martin <notmart@gmail.com>
2010-08-09 16:18:53 +0000

    a qobject that wraps some Plasma::Theme functions, will be used for bindings
    svn path=/trunk/playground/base/plasma/shells/mobile/; revision=1161067

---
+++ bindings/private/theme_p.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *   Copyright 2010 Marco Martin <mart@kde.org>                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+#ifndef THEME_PROXY_P
+#define THEME_PROXY_P
+
+#include <QObject>
+
+#include <QColor>
+
+class ThemeProxy : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QColor textColor READ textColor)
+    Q_PROPERTY(QColor highlightColor READ highlightColor)
+    Q_PROPERTY(QColor backgroundColor READ backgroundColor)
+
+public:
+    ThemeProxy(QObject *parent = 0);
+    ~ThemeProxy();
+
+    QColor textColor() const;
+    QColor highlightColor() const;
+    QColor backgroundColor() const;
+};
+
+#endif
