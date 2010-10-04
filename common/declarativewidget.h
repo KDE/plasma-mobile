055713a7c28e1e465a7b62cd74b80bf3f12ddf9c
Marco Martin <notmart@gmail.com>
2010-10-04 11:58:56 +0000

    documentation++
    svn path=/trunk/playground/base/plasma/shells/mobile/; revision=1182388

--- common/declarativewidget.h
+++ common/declarativewidget.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2010 Marco Martin <notmart@gmail.com>
+ *   Copyright 2010 Marco Martin <mart@kde.org>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as
@@ -31,35 +31,88 @@ namespace Plasma
 
 class DeclarativeWidgetPrivate;
 
-
+/**
+ * @class DeclarativeWidget plasma/declarativewidget.h <Plasma/DeclarativeWidget>
+ *
+ * @author Marco Martin <mart@kde.org>
+ *
+ * @short A widget that contains an entire QML context, with its own declarative engine
+ *
+ * Plasma::DeclarativeWidget provides a class for conveniently use QML based
+ * declarative user interfaces inside Plasma widgets.
+ * To one DeclarativeWidget corresponds one QML file (that can eventually include others)
+ * tere will be its own QDeclarativeEngine with a single root object,
+ * described in the QML file.
+ */
 class DeclarativeWidget : public QGraphicsWidget
 {
     Q_OBJECT
+
+    Q_PROPERTY(QString qmlPath READ qmlPath WRITE setQmlPath)
+    Q_PROPERTY(bool initializationDelayed READ isInitializationDelayed WRITE setInitializationDelayed)
+    Q_PROPERTY(QObject * rootObject READ rootObject)
 
 public:
 
     /**
      * Constructs a new DeclarativeWidget
      *
-     * @arg parent the parent of this widget
+     * @param parent the parent of this widget
      */
     explicit DeclarativeWidget(QGraphicsWidget *parent = 0);
     ~DeclarativeWidget();
 
+    /**
+     * Sets the path of the QML file to parse and execute
+     *
+     * @param path the absolute path of a QML file
+     */
     void setQmlPath(const QString &path);
+
+    /**
+     * @return the absolute path of the current QML file
+     */
     QString qmlPath() const;
 
+    /**
+     * Sets whether the execution of the QML file has to be delayed later in the event loop. It has to be called before setQmlPath().
+     * In this case will be possible to assign new objects in the main engine context
+     * before the main component gets initialized.
+     * So it will be possible to access it immediately from the QML code.
+     *
+     * @param delay if true the initilization of the QML file will be delayed 
+     *              at the end of the event loop
+     */
     void setInitializationDelayed(const bool delay);
+
+    /**
+     * @return true if the initilization of the QML file will be delayed 
+     *              at the end of the event loop
+     */
     bool isInitializationDelayed() const;
 
+    /**
+     * @return the declarative engine that runs the qml file assigned to this widget.
+     */
     QDeclarativeEngine* engine();
+
+    /**
+     * @return the root object of the declarative object tree
+     */
     QObject *rootObject() const;
+
+    /**
+     * @return the main QDeclarativeComponent of the engine
+     */
     QDeclarativeComponent *mainComponent() const;
 
 protected:
     void resizeEvent(QGraphicsSceneResizeEvent *event);
 
 Q_SIGNALS:
+    /**
+     * Emitted when the parsing and execution of the QML file is terminated
+     */
     void finished();
 
 private:
