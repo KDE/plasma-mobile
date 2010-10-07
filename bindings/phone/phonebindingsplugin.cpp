323f95168c67858d5077907f6f639cf46d6fb70b
Marco Martin <notmart@gmail.com>
2010-10-07 14:38:53 +0000

    make phone a plugin too (this must become a plugin at some point)
    svn path=/trunk/playground/base/plasma/shells/mobile/; revision=1183486

---
+++ bindings/phone/phonebindingsplugin.cpp
@@ -0,0 +1,37 @@
+/*
+ *   Copyright 2009 by Alan Alpert <alan.alpert@nokia.com>
+ *   Copyright 2010 by Ménard Alexis <menard@kde.org>
+ *   Copyright 2010 by Marco Martin <mart@kde.org>
+
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "phonebindingsplugin.h"
+
+#include <QtDeclarative/qdeclarative.h>
+
+#include "phone.h"
+
+void PhoneBindingsPlugin::registerTypes(const char *uri)
+{
+    Q_ASSERT(uri == QLatin1String("org.kde.plasma.phone"));
+
+    qmlRegisterType<Phone>(uri,4,7,"Phone");
+}
+
+
+#include "phonebindingsplugin.moc"
+
