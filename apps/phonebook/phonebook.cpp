8d4207b0c309e167fb05811acc701916d5a99e87
Davide Bettio <bettio@kde.org>
2011-03-22 22:03:34 +0100

    Call a contact on contact click.

--- apps/phonebook/phonebook.cpp
+++ apps/phonebook/phonebook.cpp
@@ -36,6 +36,10 @@
 #include <QAction>
 #include <KAction>
 
+#ifndef DISABLE_OFONO_QT
+#include <ofono-qt/ofonovoicecallmanager.h>
+#endif
+
 PhoneBook::PhoneBook()
 {
 
@@ -45,7 +49,14 @@ void PhoneBook::callContact(QVariant contact)
 {
     KABC::PhoneNumber::List numbersList = contact.value<KABC::Addressee>().phoneNumbers(KABC::PhoneNumber::Home);
     if (numbersList.count() > 0){
-        kDebug() << numbersList.at(0).number();
+        kDebug() << "Calling " << numbersList.at(0).number();
+
+#ifndef DISABLE_OFONO_QT
+        OfonoVoiceCallManager *callManager = new OfonoVoiceCallManager(OfonoModem::AutomaticSelect, QString(), this);
+        callManager->dial(numbersList.at(0).number(), false);
+        delete callManager;
+#endif
+        close();
     }
 }
 
