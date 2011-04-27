99fc1dd6bbbc24338abc80de55583a2b0495b4ce
Riccardo Iaconelli <riccardo@kde.org>
2011-04-27 17:35:53 +0200

    commit also the theme
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- mousetheme/build-mousetheme.sh
+++ mousetheme/build-mousetheme.sh
@@ -3,12 +3,14 @@
 mkdir -p theme;
 mkdir -p theme/cursors;
 
-for file in $(ls config/*.in); do
-    xcursorgen $file theme/cursors/$(echo $file |cut -d. -f1)
+cd config;
+for file in $(ls *.in); do
+    xcursorgen $file ../theme/cursors/$(echo $file |cut -d. -f1)
 done
+cd ..;
 
 cp index.theme theme/
 
 tar -cjf plasma-mobile-cursors.tar.bz2 theme/
-rm -rf theme;
+# rm -rf theme;
 
