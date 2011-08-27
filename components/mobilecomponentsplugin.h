a583c579bc6947ba3c8ba501e72daf9a99ef02e7
Andre Woebbeking <Woebbeking@kde.org>
2011-08-27 09:14:37 +0200

    -pedantic

--- components/mobilecomponentsplugin.h
+++ components/mobilecomponentsplugin.h
@@ -33,6 +33,6 @@ public:
     void registerTypes(const char *uri);
 };
 
-Q_EXPORT_PLUGIN2(mobilecomponentsplugin, MobileComponentsPlugin);
+Q_EXPORT_PLUGIN2(mobilecomponentsplugin, MobileComponentsPlugin)
 
 #endif
