3d1074e50df36d3065814b3b8ec8c87ce2a06e94
Sebastian Kügler <sebas@kde.org>
2011-08-27 23:09:12 +0200

    QObject-based model for URL completer

---
+++ applications/webbrowser/bookmarksmodel.cpp
@@ -0,0 +1,76 @@
+/***************************************************************************
+ *                                                                         *
+ *   Copyright 2011 Sebastian Kügler <sebas@kde.org>                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include "bookmarksmodel.h"
+
+class BookmarkPrivate {
+
+public:
+    QString name;
+    QString url;
+    QImage image;
+};
+
+
+Bookmark::Bookmark(const QString &n, const QString &u, const QImage &i, QObject *parent)
+    : QObject(parent)
+{
+    d = new BookmarkPrivate;
+    d->name = n;
+    d->url = u;
+    d->image = i;
+}
+
+Bookmark::~Bookmark()
+{
+    delete d;
+}
+
+QString Bookmark::name()
+{
+    return d->name;
+}
+
+QString Bookmark::url()
+{
+    return d->url;
+}
+
+QImage Bookmark::image()
+{
+    return d->image;
+}
+
+void Bookmark::setName(const QString &name)
+{
+    d->name = name;
+}
+
+void Bookmark::setUrl(const QString &url)
+{
+    d->url = url;
+}
+
+void Bookmark::setImage(const QImage &image)
+{
+    d->image = image;
+}
+
+#include "bookmarksmodel.moc"
---
+++ applications/webbrowser/bookmarksmodel.h
@@ -0,0 +1,63 @@
+/***************************************************************************
+ *                                                                         *
+ *   Copyright 2011 Sebastian Kügler <sebas@kde.org>                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#ifndef BOOKMARKSMODEL_H
+#define BOOKMARKSMODEL_H
+
+#include <QObject>
+#include <QImage>
+
+class BookmarkPrivate;
+
+class Bookmark : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY(QString url READ url WRITE setUrl NOTIFY urlChanged)
+    Q_PROPERTY(QImage image READ image WRITE setImage NOTIFY imageChanged)
+
+public:
+    Bookmark(const QString &name = QString(),
+             const QString &url = QString(),
+             const QImage &i = QImage(),
+             QObject *parent = 0 );
+    ~Bookmark();
+
+    QString name();
+    QString url();
+    QImage image();
+
+public Q_SLOTS:
+    void setName(const QString &n);
+    void setUrl(const QString &u);
+    void setImage(const QImage &i);
+
+Q_SIGNALS:
+    void nameChanged();
+    void urlChanged();
+    void imageChanged();
+
+private:
+    BookmarkPrivate* d;
+
+};
+
+#endif // BOOKMARKSMODEL_H
