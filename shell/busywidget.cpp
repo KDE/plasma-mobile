bcf3ada5c3b8622b449aba2128ebba5a3d23f244
Marco Martin <notmart@gmail.com>
2011-10-01 13:38:45 +0200

    everything that links into the shell as GPL
    still kcategorizeditemview remains LGPL, minor issue but would be better to relicense it since is not in a library

--- shell/busywidget.cpp
+++ shell/busywidget.cpp
@@ -2,7 +2,7 @@
  *   Copyright 2011 Marco Martin <notmart@gmail.com>
  *
  *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License version 2 as
+ *   it under the terms of the GNU General Public License version 2 as
  *   published by the Free Software Foundation
  *
  *   This program is distributed in the hope that it will be useful,
@@ -10,7 +10,7 @@
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details
  *
- *   You should have received a copy of the GNU Library General Public
+ *   You should have received a copy of the GNU General Public
  *   License along with this program; if not, write to the
  *   Free Software Foundation, Inc.,
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
--- shell/panelshadows.cpp
+++ shell/panelshadows.cpp
@@ -2,7 +2,7 @@
 *   Copyright 2011 by Aaron Seigo <aseigo@kde.org>
 *
 *   This program is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU Library General Public License version 2, 
+*   it under the terms of the GNU General Public License version 2, 
 *   or (at your option) any later version.
 *
 *   This program is distributed in the hope that it will be useful,
@@ -10,7 +10,7 @@
 *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *   GNU General Public License for more details
 *
-*   You should have received a copy of the GNU Library General Public
+*   You should have received a copy of the GNU General Public
 *   License along with this program; if not, write to the
 *   Free Software Foundation, Inc.,
 *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
