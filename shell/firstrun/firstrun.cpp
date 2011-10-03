d422da0a7b6314c6632e8b8ab4f83be630cccf6e
Sebastian Kügler <sebas@kde.org>
2011-10-03 18:13:46 +0200

    First try at a first-run app
    Should work, but needs testing.

---
+++ shell/firstrun/firstrun.cpp
@@ -0,0 +1,124 @@
+/*
+ *   Copyright 2011 Sebastian Kügler <sebas@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "firstrun.h"
+
+#include <kactivitycontroller.h>
+#include <kactivityinfo.h>
+
+#include <Nepomuk/Resource>
+#include <Nepomuk/Variant>
+
+#include <Plasma/DataEngine>
+#include <Plasma/DataEngineManager>
+
+#include <KConfig>
+#include <soprano/vocabulary.h>
+
+FirstRun::FirstRun(QObject* parent)
+    : QObject(parent),
+    m_activityController(0)
+{
+    init();
+}
+
+void FirstRun::init()
+{
+
+    KConfig* scfg = new KConfig("active-firstrunrc");
+    KConfigGroup grp = scfg->group("general");
+    bool hasRun = grp.readEntry("hasRun", false);
+    delete scfg;
+    kDebug() << "Starting first run ..." << hasRun;
+    if (!hasRun) {
+        m_activityController = new KActivityController(this);
+        m_currentActivity = m_activityController->currentActivity();
+        QStringList activities = m_activityController->listActivities();
+        //setData("allActivities", activities);
+        foreach (const QString &id, activities) {
+            kDebug() << "Activity: " << id;
+            activityAdded(id);        }
+        connect(m_activityController, SIGNAL(activityAdded(QString)), this, SLOT(activityAdded(QString)));
+    } else {
+        kDebug() << "Already ran, doing nothing";
+    }
+    kDebug() << "Done.";
+    emit done();
+    markDone();
+}
+
+FirstRun::~FirstRun()
+{
+}
+
+void FirstRun::activityAdded(const QString& source)
+{
+    kDebug() << "Source added: " << source;
+    if (!source.isEmpty()) {
+        KActivityInfo* info = new KActivityInfo(source);
+        kDebug() << "AAA: " << info->name();
+        if (info->name() == "Introduction") {
+            connectToActivity("http://en.wikipedia.org/wiki/Berlin", source, "Wikipedia: Berlin");
+            connectToActivity("http://wikitravel.org/wiki/Berlin", source, "Wikitravel: Berlin");
+            connectToActivity("http://maps.google.com", source);
+        } else if (info->name() == "My First Activity") {
+            connectToActivity("http://vizZzion.org", source, "VizZzion.org");
+        } else if (info->name() == "Vacation Planning") {
+            connectToActivity("http://seashepherd.org", source, "Seashepherd dot Org");
+        }
+    }
+}
+
+void FirstRun::connectToActivity(const QString &resourceUrl, const QString &activityId, const QString &description)
+{
+    Nepomuk::Resource fileRes(resourceUrl);
+    QUrl typeUrl;
+    kDebug() << "Adding resource " << description << " [" << resourceUrl << "] to acivity " << activityId;
+    //Bookmark?
+    if (QUrl(resourceUrl).scheme() == "http") {
+        typeUrl = QUrl("http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#Bookmark");
+        fileRes.addType(typeUrl);
+        fileRes.setDescription("Automatically added bookmark");
+        fileRes.setProperty(QUrl::fromEncoded("http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#bookmarks"), resourceUrl);
+    // App?
+    } else if (resourceUrl.endsWith(".desktop")) {
+        typeUrl = QUrl("http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#Application");
+        fileRes.addType(typeUrl);
+        KService::Ptr service = KService::serviceByDesktopPath(QUrl(resourceUrl).path());
+        if (service) {
+            fileRes.setLabel(service->name());
+            fileRes.setSymbols(QStringList() << service->icon());
+        }
+    }
+
+    Nepomuk::Resource acRes("activities://" + activityId);
+    acRes.addProperty(Soprano::Vocabulary::NAO::isRelated(), fileRes);
+}
+
+void FirstRun::markDone()
+{
+    kDebug() << "Noting in kconfig that we've run once.";
+    KConfig* scfg = new KConfig("active-firstrunrc");
+    KConfigGroup grp = scfg->group("general");
+    grp.writeEntry("hasRun", true);
+    scfg->sync();
+    delete scfg;
+}
+
+#include "firstrun.moc"
---
+++ shell/firstrun/main.cpp
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *   Copyright 2011 Sebastian Kügler <sebas@kde.org>                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include <KApplication>
+#include <KAboutData>
+#include <KCmdLineArgs>
+#include <KLocale>
+#include <KIcon>
+#include <kdebug.h>
+
+#include "app.h"
+
+static const char description[] = I18N_NOOP( "Plasma Active first run to setup Activities and Resources" );
+static const char version[] = "0.1";
+
+int main(int argc, char **argv)
+{
+    /*
+    KAboutData aboutData(argv[0], 0, ki18n("Plasma Active First Run"),
+                         version, ki18n(description), KAboutData::License_GPL,
+                         ki18n("Copyright 2011, Sebastian Kügler"));
+
+    QApplication::setGraphicsSystem("raster");
+    KCmdLineArgs::init(argc, argv, &aboutData);
+    */
+    kDebug() << "First run running ...";
+    App app(argc, argv);
+    int rc = app.exec();
+    //delete app;
+    return rc;
+    /*
+    KCmdLineOptions options;
+    options.add("nodesktop", ki18n("Starts as a normal application instead of as the primary user interface"));
+    options.add("screen <geometry>", ki18n("The geometry of the screen"), "800x480");
+#ifndef QT_NO_OPENGL
+    options.add("opengl", ki18n("use a QGLWidget for the viewport"));
+#endif
+    options.add("fullscreen", ki18n("Starts the application in fullscreen"));
+    KCmdLineArgs::addCmdLineOptions(options);
+
+    PlasmaApp *app = PlasmaApp::self();
+
+    QApplication::setWindowIcon(KIcon("plasma"));
+    app->disableSessionManagement(); // autostarted
+    int rc = app->exec();
+    delete app;
+    return rc;
+    */
+}
+
