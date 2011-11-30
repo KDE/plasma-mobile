89d26693bc07e143c0ff120b7c35ffaea401234f
Marco Martin <notmart@gmail.com>
2011-11-30 13:05:46 +0100

    delay the mouse reposition
    this is just temporary to "fix" the keyboard on touchscreen, a slightly more proper fix coming

--- virtualkeyboard/keyboardshell/plasmaapp.h
+++ virtualkeyboard/keyboardshell/plasmaapp.h
@@ -82,6 +82,7 @@ private Q_SLOTS:
     void hideKeyboard();
     void windowChangeHide();
     void clearIgnoreNextWindowHide();
+    void delayedMouseReposition();
 
 private:
     Plasma::Corona *m_corona;
