d7bccc6bbda30fe2ed6422a8a9c4f8f115e00750
Sebastian Kügler <sebas@kde.org>
2011-12-23 02:50:50 +0100

    bare-bones settings plugin
    This doesn't do much yet, but will provide loading settings modules and
    a module of available modules as Qt component.
    
    The logic comes from active-settings, which will be doable entirely in
    QML if this experiment succeeds.

---
+++ components/settings/settingsplugin.cpp
@@ -0,0 +1,35 @@
+/*
+ *   Copyright 2011 Sebastian Kügler <sebas@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Library General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "settingsplugin.h"
+#include <kdebug.h>
+
+#include <QtDeclarative/qdeclarative.h>
+
+#include "settingsmoduleloader.h"
+
+void SettingsPlugin::registerTypes(const char *uri)
+{
+    //Q_ASSERT(uri == QLatin1String("org.kde.runnermodel"));
+    kDebug() << " SettingsModulePlugin: " << uri;
+    qmlRegisterType<SettingsModuleLoader>(uri, 0, 1, "Settings");
+}
+
+#include "settingsplugin.moc"
+
