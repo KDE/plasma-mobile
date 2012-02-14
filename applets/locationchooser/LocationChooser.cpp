0145ee8e1e9305d63d015d763e2e1f0e52b1c690
Marco Martin <notmart@gmail.com>
2012-02-14 16:37:24 +0100

    name the icon plasmaapplet-location

--- applets/locationchooser/LocationChooser.cpp
+++ applets/locationchooser/LocationChooser.cpp
@@ -85,7 +85,7 @@ void LocationChooser::currentLocationChanged(const QString & id, const QString &
 void LocationChooser::popupEvent(bool show)
 {
     d->engine->requestUiReset();
-    setPopupIcon("location");
+    setPopupIcon("plasmaapplet-location");
     Plasma::PopupApplet::popupEvent(show);
 }
 
