b789c94de9c3d88e88809099b27126e30d1dde5c
Sinny Kumari <ksinny@gmail.com>
2012-03-07 22:53:50 +0530

    remove regexp as nepomuk anyway does a substring match using contains()

--- components/metadatamodel/metadatamodel.cpp
+++ components/metadatamodel/metadatamodel.cpp
@@ -303,8 +303,7 @@ void MetadataModel::doQuery()
             }
 
             //FIXME: Contains should work, but doesn't match for file names
-            Nepomuk::Query::ComparisonTerm term(propertyUrl(key), Nepomuk::Query::LiteralTerm(parameter), Nepomuk::Query::ComparisonTerm::Regexp);
-
+            Nepomuk::Query::ComparisonTerm term(propertyUrl(key), Nepomuk::Query::LiteralTerm(parameter));
             if (negation) {
                 rootTerm.addSubTerm(Nepomuk::Query::NegationTerm::negateTerm(term));
             } else {
