b17fd6c4d225e870c681f22cb34d43de596b46d8
Marco Martin <notmart@gmail.com>
2012-03-15 22:23:56 +0100

    ategorizedProxyModel is long deprecated, remove

--- components/mobilecomponents/mobilecomponentsplugin.cpp
+++ components/mobilecomponents/mobilecomponentsplugin.cpp
@@ -25,7 +25,6 @@
 #include <QDeclarativeEngine>
 
 #include "appletcontainer.h"
-#include "categorizedproxymodel.h"
 #include "pagedproxymodel.h"
 #include "fallbackcomponent.h"
 #include "package.h"
@@ -43,7 +42,6 @@ void MobileComponentsPlugin::registerTypes(const char *uri)
 
     qmlRegisterType<PagedProxyModel>(uri, 0, 1, "PagedProxyModel");
     qmlRegisterType<FallbackComponent>(uri, 0, 1, "FallbackComponent");
-    qmlRegisterType<CategorizedProxyModel>(uri, 0, 1, "CategorizedProxyModel");
     qmlRegisterType<Package>(uri, 0, 1, "Package");
     qmlRegisterType<TextEffects>(uri, 0, 1, "TextEffects");
     qmlRegisterType<AppletContainer>(uri, 0, 1, "AppletContainer");
