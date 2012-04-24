9d78a6a4dfddc3744afaa18caf8cc6e15eb9d4a4
Shantanu Tushar <shaan7in@gmail.com>
2012-04-24 23:38:32 +0530

    pkg_check_modules needs packageconfig to work properly, which we dont have (yet). So for now ActiveApp is found if the libs and headers are found.

--- applications/common/FindActiveApp.cmake
+++ applications/common/FindActiveApp.cmake
@@ -12,14 +12,6 @@ if (ACTIVEAPP_INCLUDE_DIR AND ACTIVEAPP_LIBRARIES)
   set (ACTIVEAPP_FOUND TRUE)
 
 else (ACTIVEAPP_INCLUDE_DIR AND ACTIVEAPP_LIBRARIES)
-
-  if (NOT WIN32)
-    # use pkg-config to get the values of ACTIVEAPP_INCLUDE_DIRS
-    # and ACTIVEAPP_LIBRARY_DIRS to add as hints to the find commands.
-    include (FindPkgConfig)
-    pkg_check_modules (ACTIVEAPP ActiveApp>=0.1)
-  endif (NOT WIN32)
-
   find_library (ACTIVEAPP_LIBRARIES
     NAMES
     activeapp
@@ -40,6 +32,10 @@ else (ACTIVEAPP_INCLUDE_DIR AND ACTIVEAPP_LIBRARIES)
     ${INCLUDE_INSTALL_DIR}
     ${KDE4_INCLUDE_DIR}
   )
+
+  if(ACTIVEAPP_INCLUDE_DIR AND ACTIVEAPP_LIBRARIES)
+    set(ACTIVEAPP_FOUND TRUE)
+  endif(ACTIVEAPP_INCLUDE_DIR AND ACTIVEAPP_LIBRARIES)
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(ACTIVEAPP DEFAULT_MSG ACTIVEAPP_LIBRARIES ACTIVEAPP_INCLUDE_DIR)
