6b39ce99c7123350c799fe4ebda5dcf9e6eed4a5
Marco Martin <notmart@gmail.com>
2012-08-09 17:42:19 +0200

    rename to org.kde.digital-clock for consistency

--- applets/digital-clock/Messages.sh
+++ applets/digital-clock/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_applet_org.kde.digitalclock.pot
+$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_applet_org.kde.digital-clock.pot
 rm -f rc.cpp
