38d720534071db6095d6bdfe9eebe369c73154dc
Marco Martin <notmart@gmail.com>
2012-09-03 13:06:59 +0200

    sync also when height changes

--- components/mobilecomponents/appletcontainer.cpp
+++ components/mobilecomponents/appletcontainer.cpp
@@ -188,6 +188,8 @@ void AppletContainer::afterHeightChanged()
     }
 
     m_applet.data()->resize(width(), height());
+    m_applet.data()->setPos(width()/2 - m_applet.data()->size().width()/2,
+                            height()/2 - m_applet.data()->size().height()/2);
 
     emit minimumWidthChanged(minimumWidth());
     emit preferredWidthChanged(preferredWidth());
