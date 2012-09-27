302e19d5c51968a73eaaeb90354cd600c7c27f94
Aaron Seigo <aseigo@kde.org>
2012-09-27 08:55:51 +0200

    ws changes

--- applications/settings/modules/development/sshdhelper.h
+++ applications/settings/modules/development/sshdhelper.h
@@ -14,8 +14,8 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
  */
+
 #ifndef SSHD_HELPER_H
 #define SSHD_HELPER_H
 
