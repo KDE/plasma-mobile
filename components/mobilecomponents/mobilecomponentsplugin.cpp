1fcdefe0010a8d3b7467e2db8a8fa9019364b3ab
Sebastian Kügler <sebas@kde.org>
2012-10-31 23:47:04 +0100

    remove AppletContainer from MobileComponents
    This class has been moved into the scriptengine in kde-runtime (master), and is
    available for all components as type.

--- components/mobilecomponents/mobilecomponentsplugin.cpp
+++ components/mobilecomponents/mobilecomponentsplugin.cpp
@@ -38,7 +38,6 @@ void MobileComponentsPlugin::registerTypes(const char *uri)
     qmlRegisterType<FallbackComponent>(uri, 0, 1, "FallbackComponent");
     qmlRegisterType<Package>(uri, 0, 1, "Package");
     qmlRegisterType<TextEffects>(uri, 0, 1, "TextEffects");
-    qmlRegisterType<AppletContainer>(uri, 0, 1, "AppletContainer");
 }
 
 
