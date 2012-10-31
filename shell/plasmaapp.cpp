b3ca0c33d76aa2246740740b24c3d72bf5c6a18a
Marco Martin <notmart@gmail.com>
2012-10-31 10:36:34 +0100

    get rid of the old cusom_prefix_path set in the shell

--- shell/plasmaapp.cpp
+++ shell/plasmaapp.cpp
@@ -90,7 +90,6 @@ PlasmaApp::PlasmaApp()
     KGlobal::locale()->insertCatalog("libplasma");
     KGlobal::locale()->insertCatalog("plasma-device");
 
-    setenv("PLASMA_CUSTOM_PREFIX_PATHS", "platformcontents/widget/tablet/:platformcontents/widget/touch/:platformcontents/widget/generic/:contents/", 1);
     KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
 
     Nepomuk2::ResourceManager::instance()->init();
