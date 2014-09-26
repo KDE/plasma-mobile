dd2ea9a18cba72b85b0e782f849d8a7533e390e2
Aaron Seigo <aseigo@kde.org>
2014-09-26 21:06:16 +0200

    create homescreen in plasma-shell
    requires current master branch of kconfig, plasma-framework and  plasma-worskpace

--- shell/contents/layout.js
+++ shell/contents/layout.js
@@ -1,10 +1,15 @@
-var id = createActivity("Homescreen");
+var id = currentActivity();
+if (id.length < 1) {
+    id = createActivity("Homescreen")
+}
+
 var desktopsArray = desktopsForActivity(id);
-print("Reaaaaaally?????? " + desktopsArray.length);
-for( var j = 0; j < desktopsArray.length; j++) {
-    desktopsArray[j].wallpaperPlugin = 'org.kde.color';
-    desktopsArray[j].currentConfigGroup = ["Wallpaper", "General"];
-    print("currently at ... " + desktopsArray[j].currentConfigGroup);
+for (var j = 0; j < desktopsArray.length; j++) {
+    desktopsArray[j].wallpaperPlugin = "org.kde.image";
+    desktopsArray[j].name = "Homescreen" + (j > 0 ? " " + j : "");
+    desktopsArray[j].currentConfigGroup = ["Wallpaper",
+                                           desktopsArray[j].wallpaperPlugin,
+                                           "General"];
     desktopsArray[j].writeConfig("Image", "org.kde.satellite.lockers");
 }
 
