678585e5c5966bf3cc5207ee55555642db176238
Marco Martin <notmart@gmail.com>
2014-11-20 19:51:39 +0100

    set PLASMA_PLATFORM and QT_IM_MODULE env vars

--- services/plasma-phone.service.cmake
+++ services/plasma-phone.service.cmake
@@ -10,6 +10,8 @@ Requires=dbus.socket
 
 [Service]
 Environment=DBUS_SESSION_BUS_ADDRESS=unix:path=%t/dbus/user_bus_socket
+Environment=PLASMA_PLATFORM=phone
+Environment=QT_IM_MODULE=Maliit
 ExecStart=@CMAKE_INSTALL_FULL_BINDIR@/plasma-phone
 Restart=on-failure
 
