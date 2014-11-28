245189e36b7c08619dfd26a20247c39ebc27a536
Sebastian Kügler <sebas@kde.org>
2014-11-28 02:12:47 +0100

    update copyright date

--- components/settings/settingsplugin.cpp
+++ components/settings/settingsplugin.cpp
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2011-2012 Sebastian Kügler <sebas@kde.org>
+ *   Copyright 2011-2014 Sebastian Kügler <sebas@kde.org>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as
