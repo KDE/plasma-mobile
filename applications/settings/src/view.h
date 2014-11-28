70f0a56aa4474381eca34109ceb90909a1631491
Sebastian Kügler <sebas@kde.org>
2014-11-28 02:12:54 +0100

    update copyright date

--- applications/settings/src/view.h
+++ applications/settings/src/view.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *                                                                         *
- *   Copyright 2011 Sebastian Kügler <sebas@kde.org>                       *
+ *   Copyright 2011-2014 Sebastian Kügler <sebas@kde.org>                  *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
