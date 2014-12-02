47a39df6fc56d84bbcdaa472f53fa3667b217a98
Sebastian Kügler <sebas@kde.org>
2014-12-02 04:43:11 +0100

    Update from plasma-desktop/kcms/dateandtime cd37a1ea6bf
    We may want to share the helper app, since this is just duplicated,
    security-sensitive code.

--- applications/settings/modules/time/helper.cpp
+++ applications/settings/modules/time/helper.cpp
@@ -26,13 +26,14 @@
 */
 
 #include "helper.h"
-#include "config.h"
+
+#include <config.h>
 
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
 
-//#include <kcomponentdata.h>
+#include <kcomponentdata.h>
 #include <kconfig.h>
 #include <kconfiggroup.h>
 #include <kstandarddirs.h>
@@ -40,44 +41,59 @@
 #include <QFile>
 #include <QDebug>
 
+#if defined(USE_SOLARIS)
+#include <ktemporaryfile.h>
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#endif
 
+// We cannot rely on the $PATH environment variable, because D-Bus activation
+// clears it. So we have to use a reasonable default.
+static const QString exePath = QLatin1String("/usr/sbin:/usr/bin:/sbin:/bin");
 
-int ClockHelper::ntp( const QStringList& ntpServers, bool ntpEnabled,
-                      const QString& ntpUtility )
+int ClockHelper::ntp( const QStringList& ntpServers, bool ntpEnabled )
 {
-  int ret = 0;
+    int ret = 0;
 
-  // write to the system config file
-  QFile config_file(KDE_CONFDIR "/kcmclockrc");
-  if(!config_file.exists()) {
-    config_file.open(QIODevice::WriteOnly);
-    config_file.close();
-    config_file.setPermissions(QFile::ReadOwner | QFile::WriteOwner | QFile::ReadGroup | QFile::ReadOther);
-  }
-  KConfig _config(config_file.fileName(), KConfig::SimpleConfig);
-  KConfigGroup config(&_config, "NTP");
-  config.writeEntry("servers", ntpServers );
-  config.writeEntry("enabled", ntpEnabled );
+    // write to the system config file
+    QFile config_file(KDE_CONFDIR "/kcmclockrc");
+    if(!config_file.exists()) {
+        config_file.open(QIODevice::WriteOnly);
+        config_file.close();
+        config_file.setPermissions(QFile::ReadOwner | QFile::WriteOwner | QFile::ReadGroup | QFile::ReadOther);
+    }
+    KConfig _config(config_file.fileName(), KConfig::SimpleConfig);
+    KConfigGroup config(&_config, "NTP");
+    config.writeEntry("servers", ntpServers );
+    config.writeEntry("enabled", ntpEnabled );
 
-  if ( ntpEnabled && !ntpUtility.isEmpty() ) {
-    // NTP Time setting
-    QString timeServer = ntpServers.first();
-    if( timeServer.indexOf( QRegExp(".*\\(.*\\)$") ) != -1 ) {
-      timeServer.remove( QRegExp(".*\\(") );
-      timeServer.remove( QRegExp("\\).*") );
-      // Would this be better?: s/^.*\(([^)]*)\).*$/\1/
+    QString ntpUtility = QStandardPaths::findExecutable("ntpdate");
+    if (ntpUtility.isEmpty()) {
+        ntpUtility = QStandardPaths::findExecutable("rdate");
     }
 
-    KProcess proc;
-    proc << ntpUtility << timeServer;
-    if ( proc.execute() != 0 ) {
-      ret |= NTPError;
-    }
-  } else if( ntpEnabled ) {
-    ret |= NTPError;
-  }
+    if ( ntpEnabled && !ntpUtility.isEmpty() ) {
+        // NTP Time setting
+        QString timeServer = ntpServers.first();
+        if( timeServer.indexOf( QRegExp(".*\\(.*\\)$") ) != -1 ) {
+            timeServer.replace( QRegExp(".*\\("), "" );
+            timeServer.replace( QRegExp("\\).*"), "" );
+            // Would this be better?: s/^.*\(([^)]*)\).*$/\1/
+        }
 
-  return ret;
+        KProcess proc;
+        proc << ntpUtility << timeServer;
+        if ( proc.execute() != 0 ) {
+            ret |= NTPError;
+        } else {
+            toHwclock();
+        }
+    } else if( ntpEnabled ) {
+        ret |= NTPError;
+    }
+
+    return ret;
 }
 
 int ClockHelper::date( const QString& newdate, const QString& olddate )
@@ -86,16 +102,11 @@ int ClockHelper::date( const QString& newdate, const QString& olddate )
 
     tv.tv_sec = newdate.toULong() - olddate.toULong() + time(0);
     tv.tv_usec = 0;
-#ifndef Q_OS_WIN32
     if (settimeofday(&tv, 0)) {
         return DateError;
     }
-#else
-    return DateError;
-#endif
-    if (!KStandardDirs::findExe("hwclock").isEmpty()) {
-        KProcess::execute("hwclock", QStringList() << "--systohc");
-    }
+
+    toHwclock();
     return 0;
 }
 
@@ -104,65 +115,151 @@ int ClockHelper::tz( const QString& selectedzone )
 {
     int ret = 0;
 
+    //only allow letters, numbers hyphen underscore plus and forward slash
+    //allowed pattern taken from time-util.c in systemd
+    if (!QRegExp("[a-zA-Z0-9-_+/]*").exactMatch(selectedzone)) {
+        return ret;
+    }
+
+    QString val;
+#if defined(USE_SOLARIS)	// MARCO
+    KTemporaryFile tf;
+    tf.setPrefix("kde-tzone");
+    tf.open();
+    QTextStream ts(&tf);
+
+    QFile fTimezoneFile(INITFILE);
+    bool updatedFile = false;
+
+    if (fTimezoneFile.open(QIODevice::ReadOnly))
+    {
+        bool found = false;
+
+        QTextStream is(&fTimezoneFile);
+
+        for (QString line = is.readLine(); !line.isNull();
+             line = is.readLine())
+        {
+            if (line.find("TZ=") == 0)
+            {
+                ts << "TZ=" << selectedzone << endl;
+                found = true;
+            }
+            else
+            {
+                ts << line << endl;
+            }
+        }
+
+        if (!found)
+        {
+            ts << "TZ=" << selectedzone << endl;
+        }
+
+        updatedFile = true;
+        fTimezoneFile.close();
+    }
+
+    if (updatedFile)
+    {
+        ts.device()->reset();
+        fTimezoneFile.remove();
+
+        if (fTimezoneFile.open(QIODevice::WriteOnly | QIODevice::Truncate))
+        {
+            QTextStream os(&fTimezoneFile);
+
+            for (QString line = ts->readLine(); !line.isNull();
+                 line = ts->readLine())
+            {
+                os << line << endl;
+            }
+
+            fchmod(fTimezoneFile.handle(),
+                   S_IXUSR | S_IRUSR | S_IRGRP | S_IXGRP |
+                   S_IROTH | S_IXOTH);
+            fTimezoneFile.close();
+        }
+    }
+
+
+    val = selectedzone;
+#else
     QString tz = "/usr/share/zoneinfo/" + selectedzone;
 
-    if( !KStandardDirs::findExe( "zic" ).isEmpty()) {
-        KProcess::execute("zic", QStringList() << "-l" << selectedzone);
-    } else if (!QFile::remove("/etc/localtime")) {
-        ret |= TimezoneError;
-    } else if (!QFile::copy(tz, "/etc/localtime")) {
-        ret |= TimezoneError;
+    if (QFile::exists(tz)) { // make sure the new TZ really exists
+        QFile::remove("/etc/localtime");
+    } else {
+        return TimezoneError;
+    }
+
+    if (!QFile::link(tz, "/etc/localtime")) { // fail if we can't setup the new timezone
+        return TimezoneError;
     }
 
     QFile fTimezoneFile("/etc/timezone");
 
-    if (fTimezoneFile.exists() && fTimezoneFile.open(QIODevice::WriteOnly | QIODevice::Truncate) ) {
+    if (fTimezoneFile.exists() && fTimezoneFile.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
         QTextStream t(&fTimezoneFile);
         t << selectedzone;
         fTimezoneFile.close();
     }
+#endif // !USE_SOLARIS
+    val = ':' + selectedzone;
 
-    QString val = ':' + tz;
-
-
-        setenv("TZ", val.toAscii(), 1);
-        tzset();
+    setenv("TZ", val.toLocal8Bit().constData(), 1);
+    tzset();
 
     return ret;
 }
 
 int ClockHelper::tzreset()
 {
+#if !defined(USE_SOLARIS) // Do not update the System!
+    unlink( "/etc/timezone" );
+    unlink( "/etc/localtime" );
+
+    setenv("TZ", "", 1);
+    tzset();
+#endif // !USE SOLARIS
     return 0;
+}
+
+void ClockHelper::toHwclock()
+{
+    QString hwclock = KStandardDirs::findExe("hwclock", exePath);
+    if (!hwclock.isEmpty()) {
+        KProcess::execute(hwclock, QStringList() << "--systohc");
+    }
 }
 
 ActionReply ClockHelper::save(const QVariantMap &args)
 {
-  bool _ntp = args.value("ntp").toBool();
-  bool _date = args.value("date").toBool();
-  bool _tz = args.value("tz").toBool();
-  bool _tzreset = args.value("tzreset").toBool();
+    bool _ntp = args.value("ntp").toBool();
+    bool _date = args.value("date").toBool();
+    bool _tz = args.value("tz").toBool();
+    bool _tzreset = args.value("tzreset").toBool();
 
-  //KComponentData data( "kcmdatetimehelper" );
+    KComponentData data( "activedatetimehelper" );
 
-  int ret = 0; // error code
-//  The order here is important
-  if( _ntp )
-    ret |= ntp( args.value("ntpServers").toStringList(), args.value("ntpEnabled").toBool(), args.value("ntpUtility").toString() );
-  if( _date )
-    ret |= date( args.value("newdate").toString(), args.value("olddate").toString() );
-  if( _tz )
-    ret |= tz( args.value("tzone").toString() );
-  if( _tzreset )
-    ret |= tzreset();
+    int ret = 0; // error code
+    //  The order here is important
+    if( _ntp )
+        ret |= ntp( args.value("ntpServers").toStringList(), args.value("ntpEnabled").toBool());
+    if( _date )
+        ret |= date( args.value("newdate").toString(), args.value("olddate").toString() );
+    if( _tz )
+        ret |= tz( args.value("tzone").toString() );
+    if( _tzreset )
+        ret |= tzreset();
 
-  if (ret == 0) {
-    return ActionReply::SuccessReply;
-  } else {
-    ActionReply reply(ActionReply::HelperError);
-    reply.setErrorCode(ret);
-    return reply;
-  }
+    if (ret == 0) {
+        return ActionReply::SuccessReply();
+    } else {
+        ActionReply reply(ActionReply::HelperErrorReply());
+        reply.setErrorCode(static_cast<ActionReply::Error>(ret));
+        return reply;
+    }
 }
 
-KDE4_AUTH_HELPER_MAIN("org.kde.active.clockconfig", ClockHelper)
+KAUTH_HELPER_MAIN("org.kde.active.clockconfig", ClockHelper)
--- applications/settings/modules/time/helper.h
+++ applications/settings/modules/time/helper.h
@@ -21,7 +21,7 @@
 #ifndef CLOCK_HELPER_H
 #define CLOCK_HELPER_H
 
-#include <KAuth>
+#include <kauth.h>
 
 using namespace KAuth;
 
@@ -42,11 +42,12 @@ class ClockHelper : public QObject
         ActionReply save(const QVariantMap &map);
 
     private:
-        int ntp(const QStringList& ntpServers, bool ntpEnabled,
-                const QString& ntpUtility);
+        int ntp(const QStringList& ntpServers, bool ntpEnabled);
         int date(const QString& newdate, const QString& olddate);
         int tz(const QString& selectedzone);
         int tzreset();
+
+        void toHwclock();
 };
 
 #endif // CLOCK_HELPER_H
