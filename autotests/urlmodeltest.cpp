f6b652a19368b2cc4391834c09c07f00b0ac8ac8
Sebastian Kügler <sebas@kde.org>
2014-12-12 23:38:39 +0100

    Skeleton for autotests

---
+++ autotests/urlmodeltest.cpp
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2014 Alex Richardson <arichardson.kde@gmail.com>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License version 2 as published by the Free Software Foundation;
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
+
+#include <QtTest/QTest>
+
+#include <QJsonDocument>
+#include <QFileInfo>
+#include <QJsonArray>
+
+
+class UrlModelTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void initTestCase()
+    {
+
+    };
+
+    void testModel() {
+
+    };
+};
+
+QTEST_MAIN(UrlModelTest)
+
+#include "urlmodeltest.moc"
