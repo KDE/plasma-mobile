9ae7d849a8c09fc2f5df69c38877c9812727154c
Marco Martin <notmart@gmail.com>
2017-11-08 12:25:15 +0100

    move the mobileshell import here, without paged icons

---
+++ components/fullscreenpanel.cpp
@@ -0,0 +1,51 @@
+/***************************************************************************
+ *   Copyright 2015 Marco Martin <mart@kde.org>                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include "fullscreenpanel.h"
+
+#include <QStandardPaths>
+
+#include <QDebug>
+#include <QGuiApplication>
+
+#include <kwindowsystem.h>
+
+FullScreenPanel::FullScreenPanel(QQuickWindow *parent)
+    : QQuickWindow(parent)
+{
+    setFlags(Qt::FramelessWindowHint);
+    setWindowState(Qt::WindowFullScreen);
+   // connect(this, &FullScreenPanel::activeFocusItemChanged, this, [this]() {qWarning()<<"hide()";});
+    connect(this, &QWindow::activeChanged, this, &FullScreenPanel::activeChanged);
+}
+
+FullScreenPanel::~FullScreenPanel()
+{
+}
+
+void FullScreenPanel::showEvent(QShowEvent *event)
+{
+    QQuickWindow::showEvent(event);
+    setWindowState(Qt::WindowFullScreen);
+    KWindowSystem::setState(winId(), NET::SkipTaskbar);
+}
+
+
+#include "fullscreenpanel.moc"
+
