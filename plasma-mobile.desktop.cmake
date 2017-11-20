3a73eae701cf61dafa65a196be836b8cf2ddbd5a
Bhushan Shah <bhush94@gmail.com>
2017-11-20 10:47:15 +0530

    Add Plasma Mobile session
    Summary: And also, autoload Plasma Mobile shell if session is plasma-mobile
    
    Test Plan: loaded Plasma Mobile session from the sddm
    
    Reviewers: #plasma, mart
    
    Reviewed By: #plasma, mart
    
    Subscribers: davidedmundson, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D8910

---
+++ plasma-mobile.desktop.cmake
@@ -0,0 +1,7 @@
+[Desktop Entry]
+Encoding=UTF-8
+Exec=dbus-launch --exit-with-session ${CMAKE_INSTALL_FULL_BINDIR}/startplasmacompositor
+TryExec=${CMAKE_INSTALL_FULL_BINDIR}/startplasmacompositor
+DesktopNames=KDE
+Name=Plasma Mobile
+Comment=Plasma Mobile by KDE
