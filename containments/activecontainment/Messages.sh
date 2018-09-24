f7ebb6120b36b53c969473b495c56e82ca953a2a
mrmajik45 <35823544+mrmajik45@users.noreply.github.com>
2018-09-24 09:43:07 -0400

    Use $(..) instead of legacy `..` (2)

--- containments/activecontainment/Messages.sh
+++ containments/activecontainment/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_applet_org.kde.plasma.activecontainment.pot
+$EXTRACTRC $(find . -name \*.rc -o -name \*.ui -o -name \*.kcfg) >> rc.cpp
+$XGETTEXT $(find . -name \*.qml) -L Java -o $podir/plasma_applet_org.kde.plasma.activecontainment.pot
 rm -f rc.cpp
