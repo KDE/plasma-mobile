7085526c8b5e208354b03a6ab54931914a5946b4
Marco Martin <notmart@gmail.com>
2019-09-17 11:45:01 +0200

    default setup with favorites

---
+++ look-and-feel/contents/plasmoidsetupscripts/org.kde.phone.homescreen.js
@@ -0,0 +1,5 @@
+applet.wallpaperPlugin = 'org.kde.image'
+applet.writeConfig("AppOrder", ["org.kde.phone.dialer.desktop", "org.kde.mobile.angelfish.desktop", "org.kde.phone.calindori.desktop", "org.kde.mobile.camera.desktop"])
+applet.writeConfig("Favorites", ["org.kde.phone.dialer.desktop", "org.kde.mobile.angelfish.desktop", "org.kde.phone.calindori.desktop", "org.kde.mobile.camera.desktop"])
+applet.reloadConfig()
+
