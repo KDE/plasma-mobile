f0f01e82e6adcd6b71fdecbd26e0bb66cde57445
Aleix Pol <aleixpol@kde.org>
2019-10-14 15:45:45 +0200

    Centralise startup code in this repository
    Have it start through the session file that the display manager should
    be starting.

---
+++ bin/plasma-mobile.desktop.cmake
@@ -0,0 +1,7 @@
+[Desktop Entry]
+Exec=dbus-run-session ${CMAKE_INSTALL_FULL_BINDIR}/kwinwrapper
+TryExec=${CMAKE_INSTALL_FULL_BINDIR}/kwinwrapper
+DesktopNames=KDE
+Name=Plasma Mobile
+Comment=Plasma Mobile by KDE
+X-KDE-PluginInfo-Version=${PROJECT_VERSION}
