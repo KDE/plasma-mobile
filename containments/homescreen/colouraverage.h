e055052bf3ab5634e038ed5f85b77b015ad0d1f0
Yuri Chornoivan <yurchor@ukr.net>
2020-03-15 09:28:29 +0200

    Fix minor EBN issues

--- containments/homescreen/colouraverage.h
+++ containments/homescreen/colouraverage.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2019 Carson Black                                       *
+ *   Copyright (C) 2019 Carson Black <uhhadd@gmail.com>                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -16,6 +16,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
  ***************************************************************************/
+#pragma once
 
 #include <QObject>
 #include <QColor>
