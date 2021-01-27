f19f629b4ba850ef1092fb202ded3132bf3adc73
Marco Martin <notmart@gmail.com>
2021-01-27 14:20:15 +0100

    add back panels if they disappeared

---
+++ shell/contents/updates/panelsfix.js
@@ -0,0 +1,20 @@
+let topFound = false
+let bottomFound = false
+
+for (let i in panels()) {
+    print(panels()[i].type)
+    if (panels()[i].type === "org.kde.phone.panel") {
+        topFound = true;
+    } else if (panels()[i].type === "org.kde.phone.taskpanel") {
+        topFound = true;
+    }
+}
+
+if (!topFound) {
+    let topPanel = new Panel("org.kde.phone.panel")
+    topPanel.location = "Top";
+}
+if (!bottomFound) {
+    let topPanel = new Panel("org.kde.phone.taskpanel")
+    topPanel.location = "Bottom";
+}
