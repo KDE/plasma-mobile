fefcec06c6fb82c14fd515b6c4bd6a96edc23074
Devin Lin <espidev@gmail.com>
2021-12-12 12:05:19 +0000

    homescreen: Change default favourites apps

--- look-and-feel/contents/plasmoidsetupscripts/org.kde.phone.homescreen.js
+++ look-and-feel/contents/plasmoidsetupscripts/org.kde.phone.homescreen.js
@@ -1,5 +1,5 @@
 applet.wallpaperPlugin = 'org.kde.image'
-applet.writeConfig("AppOrder", ["org.kde.phone.dialer.desktop", "org.kde.phonebook.desktop", "org.kde.mobile.angelfish.desktop", "org.kde.mobile.camera.desktop"])
-applet.writeConfig("Favorites", ["org.kde.phone.dialer.desktop", "org.kde.phonebook.desktop", "org.kde.mobile.angelfish.desktop", "org.kde.mobile.camera.desktop"])
+applet.writeConfig("AppOrder", ["org.kde.phone.dialer.desktop", "org.kde.spacebar.desktop", "org.kde.angelfish.desktop"])
+applet.writeConfig("Favorites", ["org.kde.phone.dialer.desktop", "org.kde.spacebar.desktop", "org.kde.angelfish.desktop"])
 applet.reloadConfig()
 
