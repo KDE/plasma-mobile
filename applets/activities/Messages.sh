79bf2344c7c5d2513d0cd34198b976e4e287380b
Devin Lin <espidev@gmail.com>
2022-01-19 15:49:50 -0500

    applets/activities & applets/krunner: Remove from repo
    These applets are not being used at all on the base image

--- applets/activities/Messages.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-# SPDX-FileCopyrightText: 2016 Marco Martin <mart@kde.org>
-# SPDX-License-Identifier: GPL-2.0-or-later
-
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.js -o -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_org.kde.phone.activities.pot
-rm -f rc.cpp
