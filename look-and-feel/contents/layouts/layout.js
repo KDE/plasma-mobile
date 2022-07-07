d563e1b1f887ecc18f0a2b64fce0afe4cbf971da
Devin Lin <espidev@gmail.com>
2022-07-07 22:00:42 +0000

    look-and-feel: Move settings from plasma-phone-settings to defaults
    See https://invent.kde.org/teams/plasma-mobile/issues/-/issues/148
    
    Haven't tested it yet, and I believe that some settings might not apply, have to investigate the whitelist in https://invent.kde.org/plasma/plasma-workspace/-/blob/master/kcms/lookandfeel/lookandfeelmanager.cpp
    
    CC: @apol @mart @bshah

---
+++ look-and-feel/contents/layouts/layout.js
@@ -0,0 +1,4 @@
+// SPDX-FileCopyrightText: 2022 Devin Lin <devin@kde.org>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+// no default layout, needed to have lookandfeelmanager in plasma-workspace recognize the LnF as affecting desktop layout.
