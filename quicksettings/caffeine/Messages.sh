6b843da34b5e4f72ab74fd59d610d2e4a19cb735
Devin Lin <espidev@gmail.com>
2022-12-19 12:39:51 -0500

    Ensure all packages have translations enabled

---
+++ quicksettings/caffeine/Messages.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2022 Devin Lin <devin@kde.org>
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+$XGETTEXT `find . -name \*.js -o -name \*.qml -o -name \*.cpp` -o $podir/plasma_org.kde.plasma.quicksetting.caffeine.pot
+
