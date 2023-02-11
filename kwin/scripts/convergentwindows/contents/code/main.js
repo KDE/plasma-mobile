987b280f12f3974671bf287100bdbbd0b38d0e8f
Devin Lin <espidev@gmail.com>
2023-02-11 10:04:11 -0800

    kwin/scripts: Add convergentwindows and switch from kwin maximized mode

---
+++ kwin/scripts/convergentwindows/contents/code/main.js
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: 2023 Plata Hill <plata.hill@kdemail.net>
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+function run(client) {
+  if (client.screen === 0) {
+    client.setMaximize(true, true);
+    client.noBorder = true;
+  } else {
+    client.noBorder = false;
+  }
+}
+
+workspace.clientAdded.connect((client) => {
+  if (client.normalWindow) {
+    client.clientFinishUserMovedResized.connect((client) => {
+      run(client);
+    });
+    run(client);
+  }
+});
+
+// Windows are moved from the external screen
+// to the internal screen if the external screen
+// is disconnected.
+workspace.numberScreensChanged.connect((count) => {
+  const clients = workspace.clientList();
+
+  for (var i = 0; i < clients.length; i++) {
+    if (clients[i].normalWindow) {
+      run(clients[i]);
+    }
+  }
+});
