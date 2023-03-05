2f184a5a2c4534efa357acbc2687cc4f538997a9
Devin Lin <espidev@gmail.com>
2023-03-04 18:52:41 -0800

    Fix shell layout.js

--- shell/contents/layout.js
+++ shell/contents/layout.js
@@ -8,18 +8,21 @@
 var desktopsArray = desktopsForActivity(currentActivity());
 for (var j = 0; j < desktopsArray.length; j++) {
     desktopsArray[j].wallpaperPlugin = "org.kde.image";
+
+    if (j == 0) {
+        // add meta shortcut
+        desktopsArray[0].currentConfigGroup = ["Shortcuts"]
+        desktopsArray[0].writeConfig("global", "Meta+F1")
+    }
 }
 
-// add meta shortcut 
-desktopsArray[0].currentConfigGroup = ["Shortcuts"]
-desktopsArray[0].writeConfig("global", "Meta+F1")
 
 // keep this list in sync with shell/contents/updates/panelsfix.js
-var panel = new Panel("org.kde.phone.panel");
+var panel = new Panel("org.kde.plasma.mobile.panel");
 panel.location = "top";
 panel.addWidget("org.kde.plasma.notifications");
 panel.height = 1.25 * gridUnit; // HACK: supposed to be gridUnit + smallSpacing, but it doesn't seem to give the correct number
 
-var bottomPanel = new Panel("org.kde.phone.taskpanel")
+var bottomPanel = new Panel("org.kde.desktopcontainment")
 bottomPanel.location = "bottom";
 bottomPanel.height = 2 * gridUnit;
