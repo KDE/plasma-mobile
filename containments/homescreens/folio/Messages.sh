6f296f3e4653ee2e40d10b5829f7067f94427205
Devin Lin <espidev@gmail.com>
2023-11-05 11:45:36 -0800

    homescreens/folio: Fix translations id

--- containments/homescreens/folio/Messages.sh
+++ containments/homescreens/folio/Messages.sh
@@ -4,5 +4,5 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.js -o -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_org.kde.plasma.mobile.homescreen.pot
+$XGETTEXT `find . -name \*.js -o -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_org.kde.plasma.mobile.homescreen.folio.pot
 rm -f rc.cpp
