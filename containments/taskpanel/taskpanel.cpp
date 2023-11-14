2e6b9bfb7e0dcbc9c6b616c1af76e9241f3376b4
Devin Lin <espidev@gmail.com>
2023-11-13 22:37:08 -0800

    taskpanel: Remove unused code

--- containments/taskpanel/taskpanel.cpp
+++ containments/taskpanel/taskpanel.cpp
@@ -6,21 +6,11 @@
 
 #include "taskpanel.h"
 
+#include <QDBusConnection>
 #include <QDBusPendingReply>
 #include <QDebug>
 #include <QGuiApplication>
-#include <QQuickItem>
-#include <QQuickWindow>
 #include <QtQml>
-
-#include <KWayland/Client/connection_thread.h>
-#include <KWayland/Client/output.h>
-#include <KWayland/Client/plasmashell.h>
-#include <KWayland/Client/plasmawindowmanagement.h>
-#include <KWayland/Client/registry.h>
-#include <KWayland/Client/surface.h>
-
-#include <virtualkeyboardinterface.h>
 
 // register type for Keyboards.KWinVirtualKeyboard.forceActivate();
 Q_DECLARE_METATYPE(QDBusPendingReply<>)
@@ -28,97 +18,6 @@ Q_DECLARE_METATYPE(QDBusPendingReply<>)
 TaskPanel::TaskPanel(QObject *parent, const KPluginMetaData &data, const QVariantList &args)
     : Plasma::Containment(parent, data, args)
 {
-    setHasConfigurationInterface(true);
-    initWayland();
-
-    qmlRegisterUncreatableType<KWayland::Client::Output>("org.kde.plasma.mobile.taskpanel", 1, 0, "Output", "nope");
-
-    // register type for Keyboards.KWinVirtualKeyboard.forceActivate();
-    qRegisterMetaType<QDBusPendingReply<>>();
-
-    connect(this, &Plasma::Containment::locationChanged, this, &TaskPanel::locationChanged);
-    connect(this, &Plasma::Containment::locationChanged, this, [this] {
-        auto l = location();
-        setFormFactor(l == Plasma::Types::LeftEdge || l == Plasma::Types::RightEdge ? Plasma::Types::Vertical : Plasma::Types::Horizontal);
-    });
-}
-
-void TaskPanel::initWayland()
-{
-    if (!QGuiApplication::platformName().startsWith(QLatin1String("wayland"), Qt::CaseInsensitive)) {
-        return;
-    }
-    using namespace KWayland::Client;
-    ConnectionThread *connection = ConnectionThread::fromApplication(this);
-
-    if (!connection) {
-        return;
-    }
-
-    auto *registry = new Registry(this);
-    registry->create(connection);
-
-    connect(registry, &Registry::plasmaShellAnnounced, this, [this, registry](quint32 name, quint32 version) {
-        m_shellInterface = registry->createPlasmaShell(name, version, this);
-
-        if (!m_panel) {
-            return;
-        }
-        Surface *s = Surface::fromWindow(m_panel);
-        if (!s) {
-            return;
-        }
-        m_shellSurface = m_shellInterface->createSurface(s, this);
-        m_shellSurface->setSkipTaskbar(true);
-    });
-    registry->setup();
-    connection->roundtrip();
-}
-
-QWindow *TaskPanel::panel()
-{
-    return m_panel;
-}
-
-void TaskPanel::setPanel(QWindow *panel)
-{
-    if (panel == m_panel) {
-        return;
-    }
-
-    if (m_panel) {
-        disconnect(m_panel, &QWindow::visibilityChanged, this, &TaskPanel::updatePanelVisibility);
-    }
-    m_panel = panel;
-    connect(m_panel, &QWindow::visibilityChanged, this, &TaskPanel::updatePanelVisibility, Qt::QueuedConnection);
-    Q_EMIT panelChanged();
-    updatePanelVisibility();
-}
-
-void TaskPanel::setPanelHeight(qreal height)
-{
-    if (m_panel) {
-        m_panel->setHeight(height);
-    }
-}
-
-void TaskPanel::updatePanelVisibility()
-{
-    using namespace KWayland::Client;
-    if (!m_panel->isVisible()) {
-        return;
-    }
-
-    Surface *s = Surface::fromWindow(m_panel);
-
-    if (!s) {
-        return;
-    }
-
-    m_shellSurface = m_shellInterface->createSurface(s, this);
-    if (m_shellSurface) {
-        m_shellSurface->setSkipTaskbar(true);
-    }
 }
 
 void TaskPanel::triggerTaskSwitcher() const
--- containments/taskpanel/taskpanel.h
+++ containments/taskpanel/taskpanel.h
@@ -4,56 +4,16 @@
  *   SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-#ifndef TASKPANEL_H
-#define TASKPANEL_H
+#pragma once
 
 #include <Plasma/Containment>
-#include <QWindow>
-
-class OutputsModel;
-class QAbstractItemModel;
-
-namespace KWayland
-{
-namespace Client
-{
-class Output;
-class PlasmaWindowManagement;
-class PlasmaWindow;
-class PlasmaShell;
-class PlasmaShellSurface;
-class Surface;
-}
-}
 
 class TaskPanel : public Plasma::Containment
 {
     Q_OBJECT
-    Q_PROPERTY(QWindow *panel READ panel WRITE setPanel NOTIFY panelChanged)
 
 public:
     TaskPanel(QObject *parent, const KPluginMetaData &data, const QVariantList &args);
 
-    QWindow *panel();
-    void setPanel(QWindow *panel);
-
-    Q_INVOKABLE void setPanelHeight(qreal height);
-
-    QAbstractItemModel *outputs() const;
-
     Q_INVOKABLE void triggerTaskSwitcher() const;
-
-Q_SIGNALS:
-    void panelChanged();
-    void locationChanged();
-
-private:
-    void initWayland();
-    void updatePanelVisibility();
-    QWindow *m_panel = nullptr;
-    KWayland::Client::PlasmaShellSurface *m_shellSurface = nullptr;
-    KWayland::Client::Surface *m_surface = nullptr;
-    KWayland::Client::PlasmaShell *m_shellInterface = nullptr;
 };
-
-#endif
