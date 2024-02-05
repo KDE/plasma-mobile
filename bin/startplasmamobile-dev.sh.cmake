64ca3fa307bf4bea488a8ef87cb50ddeccf6cae8
Devin Lin <espidev@gmail.com>
2024-02-05 17:47:31 +0000

    bin: Add POSIX shell linter and ensure scripts are compliant

--- bin/startplasmamobile-dev.sh.cmake
+++ bin/startplasmamobile-dev.sh.cmake
@@ -3,17 +3,17 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-source @CMAKE_INSTALL_FULL_LIBEXECDIR@/plasma-dev-prefix.sh
+. "@CMAKE_INSTALL_FULL_LIBEXECDIR@/plasma-dev-prefix.sh"
 
 # This is a bit of a hack done because systemd starts in pam, and we only set our dev paths after all that is complete
 # This copies everything into a transient runtime directory that systemd reads and reloads the units
 
-if [ ! -z  "$XDG_RUNTIME_DIR" ]; then
+if [ -n "$XDG_RUNTIME_DIR" ]; then
     mkdir -p "$XDG_RUNTIME_DIR/systemd/user.control"
-    command cp -r @KDE_INSTALL_FULL_SYSTEMDUSERUNITDIR@/* $XDG_RUNTIME_DIR/systemd/user.control
+    cp -r @KDE_INSTALL_FULL_SYSTEMDUSERUNITDIR@/* "$XDG_RUNTIME_DIR"/systemd/user.control
     systemctl --user daemon-reload
 fi
 
 
-startplasmamobile$@
+startplasmamobile "$@"
 
