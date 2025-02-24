9a35cca1b6800f08327df43d90e2bc61a52f28b7
Carl Schwan <carl@carlschwan.eu>
2025-02-24 22:58:59 +0100

    Move back wifi kcm to plasma-nm

--- kcms/wifi/Messages.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# SPDX-FileCopyrightText: 2018 Martin Kacej <m.kacej@atlas.sk>
-# SPDX-License-Identifier: LGPL-2.0-or-later
-$XGETTEXT `find . -name \*.cpp -o -name \*.qml` -o $podir/kcm_mobile_wifi.pot
